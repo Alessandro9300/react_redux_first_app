ee9095797f89fc8b908a395f4de92b44c2470311
Alessandro9300 <aandreoli93@gmail.com>
2020-09-15 12:39:31 +0200

    jsx

--- first_app/src/index.js
+++ first_app/src/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const firstH1 = React.createElement('h1', null, 'Ciao mondo!');
-const header = React.createElement('header', null, firstH1)
 
-ReactDom.render(header, document.getElementById('root'));
+
+
+// const firstH1 = React.createElement('ul',
+// {className: 'titolo'}
+// , nomi.map(nome => (React.createElement('li', null, nome))));
+//
+//
+// const header = React.createElement('header',
+// {
+//   class: 'myHeader',
+//   id: 'head'
+// }
+// , firstH1)
+
+//
+// const header = <header>
+//     <ul>
+//       {nomi.map(nome => <li>{nome}</li>)}
+//     </ul>
+//   </header>
+
+class Header extends React.Component {
+  render() {
+    const nomi = [
+      'Alessandro',
+      'Marco',
+      'Sempronio'
+    ]
+
+  return (
+    <header>
+      <div class='rightHeader'>
+        <ul>
+          {nomi.map(nome => <li> {nome}</li>)}
+        </ul>
+      </div>
+      <div class='leftHeader'>
+        <p>Padre P.</p>
+      </div>
+
+    </header>
+  )
+  }
+}
+
+
+
+
+ReactDom.render(<Header/>, document.getElementById('root'));

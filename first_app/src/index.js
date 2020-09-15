38ce65dec48ddee9ebc1c461034ed1d9fda2ff2d
Alessandro9300 <aandreoli93@gmail.com>
2020-09-15 13:21:52 +0200

    components

--- first_app/src/index.js
+++ first_app/src/index.js
@@ -3,39 +3,28 @@ import ReactDom from 'react-dom';
 
 
 
+const nomi = [
+  'Alessandro',
+  'Marco',
+  'Sempronio'
+]
 
-// const firstH1 = React.createElement('ul',
-// {className: 'titolo'}
-// , nomi.map(nome => (React.createElement('li', null, nome))));
-//
-//
-// const header = React.createElement('header',
-// {
-//   class: 'myHeader',
-//   id: 'head'
-// }
-// , firstH1)
 
-//
-// const header = <header>
-//     <ul>
-//       {nomi.map(nome => <li>{nome}</li>)}
-//     </ul>
-//   </header>
+class ListaContatti extends  React.Component {
+  render() {
+    return nomi.map(nome => <li> {nome}</li>)
+  }
+}
+
 
 class Header extends React.Component {
   render() {
-    const nomi = [
-      'Alessandro',
-      'Marco',
-      'Sempronio'
-    ]
 
   return (
     <header>
       <div class='rightHeader'>
         <ul>
-          {nomi.map(nome => <li> {nome}</li>)}
+          <ListaContatti/>
         </ul>
       </div>
       <div class='leftHeader'>
@@ -47,7 +36,45 @@ class Header extends React.Component {
   }
 }
 
+class Articolo extends React.Component{
+  render(){
+    return (
+      <div class='articolo'>
+        <h1>Sono un articolo!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    )
+  }
+}
+
+
+class Body extends React.Component{
+  render(){
+    return (
+      <main>
+        <Articolo/>
+        <Articolo/>
+        <Articolo/>
+        <Articolo/>
+
+      </main>
+    )
+  }
+}
+
+class Main extends React.Component {
+  render(){
+    return (
+      <div class='body'>
+        <Header/>
+        <Body/>
+      </div>
+    )
+  }
+}
 
 
 
-ReactDom.render(<Header/>, document.getElementById('root'));
+ReactDom.render(<Main/>, document.getElementById('root'));
